986e52c31c4b035a9f4ff6aa57587fea99ef85c2
Ely <scoofy@gmail.com>
2015-09-09 11:46:46 +0200

    Hallo iedereen

--- src/gitcursus/GitCursus.java
+++ src/gitcursus/GitCursus.java
@@ -16,7 +16,7 @@ public class GitCursus {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Boshoer");
+        System.out.println("Hallo iedereen");
     }
     
 }
